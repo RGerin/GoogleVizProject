16969cdc033e1b0c5fe07c124d0b8b918fe30b1c
Roseanne Gerin <rg2875@columbia.edu>
2014-02-28 19:33:03 -0500

    Fifth Commit

--- Scripts.js
+++ Scripts.js
@@ -15,26 +15,26 @@
 //I must remember to check the web page only in Firefox and not Chrome to ensure the data has been loaded.
 
 function infoLoaded(joblessInfo){
-	
+
 	console.log(joblessInfo);
-	
+
 	//I will add a variable to put a lable on "Observations" in the data in my JSON file.
-	
+
 	var observationsArray = joblessInfo.observations;
-	
+
 		console.log(joblessInfo.observations);
-		
+
 	//Now I'm going to create a container to hold all the other lists.
-	
+
 	var masterList = [];
-	
+
 	//I must create a var with a "new header" list to adjust the "Unknown header type 548" 
 	//line error that appears in Firefox.
 	var newHeader = ["Date", "Jobless Data"];
-	
+
 	masterList.push(newHeader);
-	
-		
+
+
 	//To create my visualiazation, I need to convert my JSON data to an array of arrays.
 	//To do this, I will have to use for loops to loop individual data point arrays
 	//from the "date" and "value" data and use it to drive the visualization. 
@@ -45,7 +45,7 @@ function infoLoaded(joblessInfo){
 	//Final thing is to put the data array into the data list.
 	//Then the masterList should be populated with an array of arrays that is all
 	//the observations containing only the "date" and "value."
-	
+
 	for(var i=0; i<observationsArray.length; i++) {
 		var indieObject = observationsArray[i];	
 		var mainDataArray = [indieObject.date, Number(indieObject.value)];	
@@ -55,18 +55,18 @@ function infoLoaded(joblessInfo){
 	var num = new Number(100);
 
 	console.log(masterList);
-		
+
 	//I am creating a variable for the chart title with a title property.
 	//This will be the header of my chart.
 	var options = {
 		title: "Acme Shoe Company Performance"
 	};
-		
+
 	console.log(masterList);
-	
+
 	//Now I am going to use an array to data table function and pass the set of arrays 
 	//I created to generate the data table to pass to the visualization function.
-	
+
 	var mainDataTable = google.visualization.arrayToDataTable(masterList);
 	//I'm going to create a chart variable. The document.getElementByID is the 
 	//equivalent of the jQuery $("#divName"). I must add the raw JavaScript command
@@ -84,7 +84,7 @@ function googleProjectLoaded(){
  "$.get is the "Marco" and "infoLoaded"" is the "Polo."
  */
 		$.get("GoogleChart.json", infoLoaded, "json");	
-		
+
 }
 
 
@@ -95,7 +95,7 @@ function pageReady(){
 	//followed by a list of parameters. The google name is the "Marco" and the function name is the "Polo."
 	google.load("visualization", "1", {packages:["corechart"], "callback":googleProjectLoaded})
 
-	
+
 }
 
 /*I will add a callback name via a document ready function.
